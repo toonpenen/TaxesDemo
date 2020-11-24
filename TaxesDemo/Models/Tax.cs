a12c41ddd9e5403d69795fb2cdf7a24c07a52260
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-24 11:01:34 +0100

    Add project files.

---
+++ TaxesDemo/Models/Tax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxesDemo.Models
+{
+    public abstract class Tax
+    {
+        public abstract void CalculateTax();
+    }
+}
